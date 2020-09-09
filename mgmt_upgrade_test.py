73feda34281fdc32f0ddbdf01d7861a521d1c504
Shlomi Balalis <Balalis@scylladb.com>
2020-09-09 03:29:12 +0300

    test(manager): TESTSSSSSSSSSSSSSSSSSSS

--- mgmt_upgrade_test.py
+++ mgmt_upgrade_test.py
@@ -2,7 +2,7 @@ import logging
 from time import sleep
 
 from sdcm.tester import ClusterTester
-from sdcm.mgmt import get_scylla_manager_tool, TaskStatus, update_config_file
+from sdcm.mgmt import get_scylla_manager_tool, TaskStatus, update_config_file, RepairTask
 from mgmt_cli_test import BackupFunctionsMixIn
 
 
@@ -51,39 +51,50 @@ class ManagerUpgradeTest(BackupFunctionsMixIn, ClusterTester):
 
         mgr_cluster = manager_tool.get_cluster(cluster_name="cluster_under_test")
 
-        with self.subTest("Creating reoccurring backup and repair tasks"):
+        # with self.subTest("Creating reoccurring backup and repair tasks"):
+        #
+        #     repair_task = mgr_cluster.create_repair_task(interval="1d")
+        #     repair_task_current_details = wait_until_task_finishes_return_details(repair_task)
+        #
+        #     self.update_all_agent_config_files()
+        #     bucket_name = self.params.get('backup_bucket_location').split()[0]
+        #     location_list = [f's3:{bucket_name}']
+        #     backup_task = mgr_cluster.create_backup_task(interval="1d", location_list=location_list,
+        #                                                  keyspace_list=["keyspace1"])
+        #     backup_task_current_details = wait_until_task_finishes_return_details(backup_task)
+        #     backup_task_snapshot = backup_task.get_snapshot_tag()
+        #     pre_upgrade_backup_task_files = mgr_cluster.get_backup_files_dict(backup_task_snapshot)
+        #
+        # with self.subTest("Creating a backup task and stopping it"):
+        #     pausable_backup_task = mgr_cluster.create_backup_task(interval="1d", location_list=location_list,
+        #                                                           keyspace_list=["system_*"])
+        #     pausable_backup_task.wait_for_status(list_status=[TaskStatus.RUNNING], timeout=180, step=2)
+        #     pausable_backup_task.stop()
+        #
+        # with self.subTest("Creating a simple backup with the intention of purging it"):
+        #     self.create_simple_table(table_name="cf1")
+        #     self.write_multiple_rows(table_name="cf1", key_range=(1, 11))
+        #     self.create_simple_table(table_name="cf2")
+        #     self.write_multiple_rows(table_name="cf2", key_range=(1, 11))
+        #
+        #     bucket_name = self.params.get('backup_bucket_location').split()[0]
+        #     location_list = [f's3:{bucket_name}']
+        #     rerunning_backup_task = mgr_cluster.create_backup_task(location_list=location_list, keyspace_list=["ks1"],
+        #                                                            retention=2)
+        #     rerunning_backup_task.wait_and_get_final_status(timeout=300, step=20)
+        #     assert rerunning_backup_task.status == TaskStatus.DONE, \
+        #         f"Unknown failure in task {rerunning_backup_task.id}"
 
-            repair_task = mgr_cluster.create_repair_task(interval="1d")
-            repair_task_current_details = wait_until_task_finishes_return_details(repair_task)
-
-            self.update_all_agent_config_files()
-            bucket_name = self.params.get('backup_bucket_location').split()[0]
-            location_list = [f's3:{bucket_name}']
-            backup_task = mgr_cluster.create_backup_task(interval="1d", location_list=location_list,
-                                                         keyspace_list=["keyspace1"])
-            backup_task_current_details = wait_until_task_finishes_return_details(backup_task)
-            backup_task_snapshot = backup_task.get_snapshot_tag()
-            pre_upgrade_backup_task_files = mgr_cluster.get_backup_files_dict(backup_task_snapshot)
-
-        with self.subTest("Creating a backup task and stopping it"):
-            pausable_backup_task = mgr_cluster.create_backup_task(interval="1d", location_list=location_list,
-                                                                  keyspace_list=["system_*"])
-            pausable_backup_task.wait_for_status(list_status=[TaskStatus.RUNNING], timeout=180, step=2)
-            pausable_backup_task.stop()
-
-        with self.subTest("Creating a simple backup with the intention of purging it"):
-            self.create_simple_table(table_name="cf1")
-            self.write_multiple_rows(table_name="cf1", key_range=(1, 11))
-            self.create_simple_table(table_name="cf2")
-            self.write_multiple_rows(table_name="cf2", key_range=(1, 11))
-
-            bucket_name = self.params.get('backup_bucket_location').split()[0]
-            location_list = [f's3:{bucket_name}']
-            rerunning_backup_task = mgr_cluster.create_backup_task(location_list=location_list, keyspace_list=["ks1"],
-                                                                   retention=2)
-            rerunning_backup_task.wait_and_get_final_status(timeout=300, step=20)
-            assert rerunning_backup_task.status == TaskStatus.DONE, \
-                f"Unknown failure in task {rerunning_backup_task.id}"
+        with self.subTest("Creating repairs using with-host, host and token-ranges"):
+            legacy_args = [
+                f" --host {self.db_cluster.nodes[0].ip_address}",
+                f" --with-hosts {self.db_cluster.nodes[0].ip_address}",
+                f" --token-ranges all --host {self.db_cluster.nodes[0].ip_address}"
+            ]
+            legacy_args_tasks = {}
+            for arg in legacy_args:
+                legacy_args_tasks[arg] = _create_stopped_repair_task_with_manual_argument(
+                    mgr_cluster=mgr_cluster, arg_string=arg)
 
         upgrade_scylla_manager(pre_upgrade_manager_version=current_manager_version,
                                target_upgrade_server_version=target_upgrade_server_version,
@@ -95,41 +106,56 @@ class ManagerUpgradeTest(BackupFunctionsMixIn, ClusterTester):
         manager_tool = get_scylla_manager_tool(manager_node=manager_node)
         # make sure that the cluster is still added to the manager
         manager_tool.get_cluster(cluster_name="cluster_under_test")
-        validate_previous_task_details(task=repair_task, previous_task_details=repair_task_current_details)
-        validate_previous_task_details(task=backup_task, previous_task_details=backup_task_current_details)
+        # validate_previous_task_details(task=repair_task, previous_task_details=repair_task_current_details)
+        # validate_previous_task_details(task=backup_task, previous_task_details=backup_task_current_details)
 
-        with self.subTest("Restoring a 2.0 backup task with 2.1 manager"):
-            self.verify_backup_success(mgr_cluster=mgr_cluster, backup_task=backup_task)
+        # with self.subTest("Restoring a 2.0 backup task with 2.1 manager"):
+        #     self.verify_backup_success(mgr_cluster=mgr_cluster, backup_task=backup_task)
+        #
+        # with self.subTest("Continuing a 2.0 stopped backup task with 2.1 manager"):
+        #     pausable_backup_task.start()
+        #     pausable_backup_task.wait_and_get_final_status(timeout=1200, step=20)
+        #     assert pausable_backup_task.status == TaskStatus.DONE, \
+        #         f"task {pausable_backup_task.id} failed to continue after manager upgrade"
+        #
+        # with self.subTest("Executing the 'backup list' and 'backup files' commands on a 2.0 backup with 2.1 manager"):
+        #     current_backup_files = mgr_cluster.get_backup_files_dict(backup_task_snapshot)
+        #     assert pre_upgrade_backup_task_files == current_backup_files,\
+        #         f"Backup task of the task {backup_task.id} is not identical after the manager upgrade:" \
+        #         f"\nbefore the upgrade:\n{pre_upgrade_backup_task_files}\nafter the upgrade:\n{current_backup_files}"
+        #     mgr_cluster.sctool.run(cmd=f" backup list -c {mgr_cluster.id}", is_verify_errorless_result=True)
+        #
+        # with self.subTest("purging a 2.0 backup"):
+        #     # Dropping one table
+        #     with self.db_cluster.cql_connection_patient(self.db_cluster.nodes[0]) as session:
+        #         session.execute("DROP TABLE ks1.cf1 ;")
+        #
+        #     for i in range(2, 4):
+        #         LOGGER.debug(f"rerunning the backup task for the {i} time")
+        #         rerunning_backup_task.start(continue_task=False)
+        #     per_node_backup_file_paths = mgr_cluster.get_backup_files_dict(
+        #         snapshot_tag=rerunning_backup_task.get_snapshot_tag())
+        #     for node in self.db_cluster.nodes:
+        #         nodetool_info = self.db_cluster.get_nodetool_info(node)
+        #         node_id = nodetool_info['ID']
+        #         # making sure that the files of the missing table isn't in s3
+        #         assert "cf1" not in per_node_backup_file_paths[node_id]["ks1"], \
+        #             "The missing table is still in s3, even though it should have been purged"
 
-        with self.subTest("Continuing a 2.0 stopped backup task with 2.1 manager"):
-            pausable_backup_task.start()
-            pausable_backup_task.wait_and_get_final_status(timeout=1200, step=20)
-            assert pausable_backup_task.status == TaskStatus.DONE, \
-                f"task {pausable_backup_task.id} failed to continue after manager upgrade"
+        with self.subTest("Rerunning legacy repairs"):
+            for original_arg_string, task in legacy_args_tasks.items():
+                task.start()
+                task.wait_and_get_final_status(timeout=5000, step=10)
 
-        with self.subTest("Executing the 'backup list' and 'backup files' commands on a 2.0 backup with 2.1 manager"):
-            current_backup_files = mgr_cluster.get_backup_files_dict(backup_task_snapshot)
-            assert pre_upgrade_backup_task_files == current_backup_files,\
-                f"Backup task of the task {backup_task.id} is not identical after the manager upgrade:" \
-                f"\nbefore the upgrade:\n{pre_upgrade_backup_task_files}\nafter the upgrade:\n{current_backup_files}"
-            mgr_cluster.sctool.run(cmd=f" backup list -c {mgr_cluster.id}", is_verify_errorless_result=True)
-
-        with self.subTest("purging a 2.0 backup"):
-            # Dropping one table
-            with self.db_cluster.cql_connection_patient(self.db_cluster.nodes[0]) as session:
-                session.execute("DROP TABLE ks1.cf1 ;")
-
-            for i in range(2, 4):
-                LOGGER.debug(f"rerunning the backup task for the {i} time")
-                rerunning_backup_task.start(continue_task=False)
-            per_node_backup_file_paths = mgr_cluster.get_backup_files_dict(
-                snapshot_tag=rerunning_backup_task.get_snapshot_tag())
-            for node in self.db_cluster.nodes:
-                nodetool_info = self.db_cluster.get_nodetool_info(node)
-                node_id = nodetool_info['ID']
-                # making sure that the files of the missing table isn't in s3
-                assert "cf1" not in per_node_backup_file_paths[node_id]["ks1"], \
-                    "The missing table is still in s3, even though it should have been purged"
+                current_arg_string = task.arguments
+                assert task.status == TaskStatus.DONE, f"Task {task.id} with legacy arg '{original_arg_string}' did " \
+                                                       f"not end successfully in the expected time after the upgrade." \
+                                                       f"\nThe arguments string of the task after the upgrade is " \
+                                                       f"'{current_arg_string}'"
+                assert not current_arg_string, f"Task {task.id}, which was created before the upgrade with the legacy" \
+                                               f" arg '{original_arg_string}' Was expected to contain an empty arg " \
+                                               f"string after the upgrade, but instead the current arg string" \
+                                               f" is '{current_arg_string}'"
 
     def update_all_agent_config_files(self):
         config_file = '/etc/scylla-manager-agent/scylla-manager-agent.yaml'
@@ -214,3 +240,15 @@ def upgrade_scylla_manager(pre_upgrade_manager_version, target_upgrade_server_ve
     new_manager_version = manager_tool.version
     assert new_manager_version != pre_upgrade_manager_version, "Manager failed to upgrade - " \
                                                                "previous and new versions are the same. Test failed!"
+
+
+def _create_stopped_repair_task_with_manual_argument(mgr_cluster, arg_string):
+    res = mgr_cluster.sctool.run(cmd=f"repair -c {mgr_cluster.id} {arg_string}",
+                                     parse_table_res=False)
+    assert not res.stderr, f"Task creation failed: {res.stderr}"
+    task_id = res.stdout.split('\n')[0]
+    repair_task = RepairTask(task_id=task_id, cluster_id=mgr_cluster.id,
+                             manager_node=mgr_cluster.manager_node)
+    repair_task.wait_for_status(list_status=[TaskStatus.RUNNING], timeout=500, step=10)
+    repair_task.stop()
+    return repair_task
