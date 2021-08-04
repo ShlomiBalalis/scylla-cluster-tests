6e6d0650e6a4e6c946cfcdae3b1205914fca72c0
Shlomi Balalis <Balalis@scylladb.com>
2021-08-04 16:14:16 +0300

    fix(disrupt_network_start_stop_interface): modified to suit ubuntu
    Since the ifup and ifdown commands do not exist in ubuntu, I added an installation
    of ifconfig, that will be used instead of said commands.

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -2379,11 +2379,17 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         wait_time = random.choice(list_of_timeout_options)
         self.log.debug("Taking down eth1 for %dsec", wait_time)
 
+        if self.target_node.distro.is_rhel_like:
+            shutdown_interface_command = "/sbin/ifdown {}"
+            startup_interface_command = "/sbin/ifup {}"
+        else:
+            shutdown_interface_command = "ifconfig {} down"
+            startup_interface_command = "ifconfig {} up"
         try:
-            self.target_node.remoter.run("sudo /sbin/ifdown eth1")
+            self.target_node.remoter.sudo(shutdown_interface_command.format("eth1"))
             time.sleep(wait_time)
         finally:
-            self.target_node.remoter.run("sudo /sbin/ifup eth1")
+            self.target_node.remoter.sudo(startup_interface_command.format("eth1"))
             self._wait_all_nodes_un()
 
     def break_streaming_task_and_rebuild(self, task='decommission'):  # pylint: disable=too-many-statements
