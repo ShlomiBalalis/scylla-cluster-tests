e490196bd582c1d483fdaeeb2a61373a5dc60843
Shlomi Balalis <Balalis@scylladb.com>
2022-05-18 18:47:11 +0300

    test(manager): Added sanity and upgrade tests for ubuntu22 and debian11

--- sdcm/utils/distro.py
+++ sdcm/utils/distro.py
@@ -44,6 +44,7 @@ class Distro(enum.Enum):
     UBUNTU20 = ("ubuntu", "20.04")
     UBUNTU21 = ("ubuntu", "21.04")
     UBUNTU21_10 = ("ubuntu", "21.10")
+    UBUNTU22 = ("ubuntu", "22.04")
     SLES15 = ("sles", "15")
 
     @classmethod
