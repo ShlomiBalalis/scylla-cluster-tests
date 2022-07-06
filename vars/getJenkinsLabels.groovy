6282e18068d50f501f5dd55cbf712a09313c440b
Shlomi Balalis <Balalis@scylladb.com>
2022-07-06 11:02:58 +0300

    fix(gce datacenter): gce datacenter is now a multi region param

--- vars/getJenkinsLabels.groovy
+++ vars/getJenkinsLabels.groovy
@@ -8,6 +8,12 @@ def call(String backend, String region=null, String datacenter=null, String loca
     } catch(Exception) {
 
     }
+    try {
+        datacenterList = new JsonSlurper().parseText(datacenter)
+        datacenter = datacenterList[0]
+    } catch(Exception) {
+
+    }
 
     def jenkins_labels = ['aws-eu-west-1': 'aws-sct-builders-eu-west-1-new',
                           'aws-eu-west-2': 'aws-sct-builders-eu-west-2',
