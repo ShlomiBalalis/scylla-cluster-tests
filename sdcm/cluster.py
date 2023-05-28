4910c42947e81229146a9123941a89f6a358d612
Shlomi Balalis <Balalis@scylladb.com>
2023-05-28 19:44:55 +0300

    feature(perftune): testing the output of perftune.py after node setup
    https://github.com/scylladb/scylla-enterprise/issues/2909

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -104,6 +104,7 @@ from sdcm.utils.version_utils import (
     ComparableScyllaVersion,
     SCYLLA_VERSION_RE,
 )
+from sdcm.utils.perftune_validator import PerftuneOutputChecker
 from sdcm.sct_events import Severity
 from sdcm.sct_events.base import LogEvent, add_severity_limit_rules, max_severity
 from sdcm.sct_events.health import ClusterHealthValidatorEvent
@@ -4436,6 +4437,8 @@ class BaseScyllaCluster:  # pylint: disable=too-many-public-methods, too-many-in
         nodes_status = node.get_nodes_status()
         check_nodes_status(nodes_status=nodes_status, current_node=node)
 
+        perftune_checker = PerftuneOutputChecker(node)
+        perftune_checker.compare_perftune_results()
         self.clean_replacement_node_options(node)
 
     def install_scylla_manager(self, node):
