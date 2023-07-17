746f2c357f2667856a753b36e44f581bbd19baff
Shlomi Balalis <Balalis@scylladb.com>
2023-07-17 17:43:30 +0300

    fix(argus report): include instance type

--- vars/createArgusTestRun.groovy
+++ vars/createArgusTestRun.groovy
@@ -1,7 +1,8 @@
 #!groovy
 
-def call(Map params) {
+def call(Map params, String instanceType) {
     def test_config = groovy.json.JsonOutput.toJson(params.test_config)
+
     sh """#!/bin/bash
         set -xe
 
@@ -9,6 +10,19 @@ def call(Map params) {
 
         export SCT_CLUSTER_BACKEND="${params.backend}"
         export SCT_CONFIG_FILES=${test_config}
+
+        case "${params.backend}" in
+            "aws")
+                export SCT_INSTANCE_TYPE_DB="${instanceType}"
+                ;;
+            "gce")
+                export SCT_GCE_INSTANCE_TYPE_DB="${instanceType}"
+                ;;
+            "azure")
+                export SCT_AZURE_INSTANCE_TYPE_DB="${instanceType}"
+                ;;
+        esac
+
 
         ./docker/env/hydra.sh create-argus-test-run
 
